5042189227bc669047af131a9727ba8c2ce83a4c
Shay Sheller <shaysheller@outlook.com>
2023-11-29 18:02:01 -0800

    deleted a few items no functionality changed

--- src/server/routes/userApi.ts
+++ src/server/routes/userApi.ts
@@ -7,19 +7,25 @@ import initController from '../controllers/initController.js';
 const router = express.Router();
 
 // Get request to grab all users and return it
-router.get('/all', userController.getAllUsers, (req,res) => {
+router.get('/all', userController.getAllUsers, (req, res) => {
   return res.status(200).json(res.locals.allUsers);
 });
 
 // Post request to handle user verification setting cookie and logging the user in
-router.post('/login', userController.verifyUser, cookieController.setCookie, userController.checkEnv, (req, res) => {
-  const obj = {
-    grafUrl: res.locals.grafUrl,
-    key: res.locals.apiKey
-  };
+router.post(
+  '/login',
+  userController.verifyUser,
+  cookieController.setCookie,
+  userController.checkEnv,
+  (req, res) => {
+    const obj = {
+      grafUrl: res.locals.grafUrl,
+      key: res.locals.apiKey,
+    };
 
-  return res.status(200).json(obj);
-});
+    return res.status(200).json(obj);
+  }
+);
 
 // Post request which allows us to update our env file
 router.post('/env', initController.updateEnv, (req, res) => {
@@ -27,19 +33,36 @@ router.post('/env', initController.updateEnv, (req, res) => {
 });
 
 // Post request which manages signing up specifically the admin user
-router.post('/signupAdmin', userController.encrypt, userController.createAdminUser, userController.createAdminUserInfo, (req, res) => {
-  return res.status(200).json('successful');
-});
+router.post(
+  '/signupAdmin',
+  userController.encrypt,
+  userController.createAdminUser,
+  userController.createAdminUserInfo,
+  (req, res) => {
+    return res.status(200).json('successful');
+  }
+);
 
 // Post request which manages signing up a new user account and encrypting their password
-router.post('/signup', userController.encrypt, userController.createUser, (req, res) => {
-  return res.status(200).json('successful');
-});
+router.post(
+  '/signup',
+  userController.encrypt,
+  userController.createUser,
+  (req, res) => {
+    return res.status(200).json('successful');
+  }
+);
 
 // Patch request which will allow the user to change their password (old password must be verified first)
-router.patch('/', userController.verifyUser, userController.encrypt, userController.updateUser, (req, res) => {
-  return res.status(200).json('successful');
-});
+router.patch(
+  '/',
+  userController.verifyUser,
+  userController.encrypt,
+  userController.updateUser,
+  (req, res) => {
+    return res.status(200).json('successful');
+  }
+);
 
 // Delete request which manages deleting a user
 router.delete('/', userController.deleteUser, (req, res) => {
